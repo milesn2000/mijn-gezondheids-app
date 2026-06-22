c4acb149e96143cf7cde49662f2693fa8fed8b74
milesn2000 <miles.nouwens@edu-kw1c.nl>
2026-06-22 09:47:21 +0200

    manifest afgemaakt

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app     = express();
 const PORT    = 3000;
 // Stap 2: Een route maken
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Stap 3: Server starten
 app.listen(PORT, () => 
